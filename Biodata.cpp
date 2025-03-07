29a1ea7d11b388a39139418828cb8070f9b0a171
Avvun Erune <awan.cahya07@gmail.com>
2025-03-07 20:33:15 +0700

    Percobaan upload kedua

---
+++ Biodata.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "+-------------------------------+\n";
+    cout << "| Universitas AMIKOM Yogyakarta |\n";
+    cout << "|     Creative Economy Park     |\n";
+    cout << "+-------------------------------+\n\n";
+
+    cout << "Nama: Anugrah Awan Cahya Putra\n";
+    cout << "NIM: 23.11.5380\n";
+    cout << "Jurusan: Informatika\n";
+    cout << "Asal: Yogyakarta" << endl;
+}
